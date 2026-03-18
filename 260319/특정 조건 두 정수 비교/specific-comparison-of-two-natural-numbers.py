6881595abc9057f00b4f2f4fb84a3324187e5ae2
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-03-19 01:43:42 +0900

    Title: 특정 조건 두 정수 비교, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260319/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,11 @@
+num1, num2 = map(int, input().split())
+if num1 < num2:
+    print(1, end=" ")
+else:
+   print(0, end=" ")
+
+if num1 == num2:
+    print(1, end=" ")
+else:
+    print(0, end=" ")
+
