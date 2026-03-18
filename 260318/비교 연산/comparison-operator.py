0d4bc067977d59f42c37fe8b6e411eed5426ab0c
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-03-18 13:07:35 +0900

    Title: 비교 연산, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260318/비교 연산/comparison-operator.py
@@ -0,0 +1,19 @@
+A, B = map(int, input().split())
+
+# 1. A >= B
+print(1 if A >= B else 0)
+
+# 2. A > B
+print(1 if A > B else 0)
+
+# 3. B >= A
+print(1 if B >= A else 0)
+
+# 4. B > A
+print(1 if B > A else 0)
+
+# 5. A == B
+print(1 if A == B else 0)
+
+# 6. A != B
+print(1 if A != B else 0)
