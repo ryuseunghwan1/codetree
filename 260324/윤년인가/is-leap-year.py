5bbb1ca5c41e0a7ad6461a91ea0459f7d6d813ca
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-03-24 03:01:45 +0900

    Title: 윤년인가, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260324/윤년인가/is-leap-year.py
+++ 260324/윤년인가/is-leap-year.py
@@ -1,7 +1,9 @@
-four_year = int(input())
 
-if four_year % 4 == 0:
-    if not (four_year % 100 == 0) and (four_year % 400 > 0):
-        print("true")
+year = int(input())
+
+# 1. 4의 배수이고, 100의 배수가 아닐 때 -> 윤년
+# 2. 또는 400의 배수일 때 -> 윤년
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print("true")
 else:
     print("false")
