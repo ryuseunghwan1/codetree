a1bc007eb12cc10b9f7e3566c836995867a1b995
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-03-24 02:58:38 +0900

    Title: 윤년인가, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260324/윤년인가/is-leap-year.py
+++ 260324/윤년인가/is-leap-year.py
@@ -3,5 +3,5 @@ four_year = int(input())
 if four_year % 4 == 0:
     if not (four_year % 100 == 0) and (four_year % 400 > 0):
         print("true")
-    else:
-        print("false")
+else:
+    print("false")
