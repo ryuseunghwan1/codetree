93f8063eff1b7ed375e355c8b619c888e3621ba8
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-03-28 01:05:25 +0900

    Title: 배수의 개수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260328/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,13 @@
+three = 0
+five = 0
+
+for _ in range(10):
+    num = int(input())
+    
+    if num % 3 == 0:
+        three += 1
+        
+    if num % 5 == 0:
+        five += 1
+
+print(three, five)
