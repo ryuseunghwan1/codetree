0d035b260edd73aff2f23cd6554021b6c1939838
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-03-29 00:38:39 +0900

    Title: A 부터 B 까지, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260329/A 부터 B 까지/a-to-b.py
+++ 260329/A 부터 B 까지/a-to-b.py
@@ -3,12 +3,12 @@ num1, num2 = map(int, input().split())
 while num1 >= 0:
     
     print(num1, end=" ")
-    if num1 % 2 == 0:
+    
+    if num1 % 2 == 0: 
         num1 += 3 
     else:
         num1 *= 2
         
-    if num1 >= num2:
-        print(num2)
+    if num1 > num2:
         break
     
