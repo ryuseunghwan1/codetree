c3157fadd23ee5387f41945735002bf908fa0258
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-04 02:59:48 +0900

    Title: 출석 부르기 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260404/출석 부르기 2/calling-attendance-2.py
@@ -0,0 +1,14 @@
+while True:
+    N = int(input())
+    
+    if N == 1:
+        print('John')
+    elif N == 2:
+        print('Tom')
+    elif N == 3:
+        print('Paul')
+    elif N == 4:
+        print('Sam')
+    else:
+        print('Vacancy')
+        break
