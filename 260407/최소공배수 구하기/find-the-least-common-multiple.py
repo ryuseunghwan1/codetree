fb54e474e8dbfd68be2b72bf4f8e50e49c3b9c45
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-07 01:54:57 +0900

    Title: 최소공배수 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260407/최소공배수 구하기/find-the-least-common-multiple.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+
+# 출력
+print(lcm(n, m))
