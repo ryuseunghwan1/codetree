19ddd397fe2a15f32207b29b40906afdfaf60353
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-20 00:55:33 +0900

    Title: 알파벳 출력하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260420/알파벳 출력하기/print-alphabet.py
@@ -0,0 +1,11 @@
+N = int(input())
+cnt = 0
+
+for i in range(1, N+1):
+    for j in range(i):
+        print(chr(65+cnt), end='')
+        cnt += 1
+
+        if cnt == 26:
+            cnt = 0
+    print()
