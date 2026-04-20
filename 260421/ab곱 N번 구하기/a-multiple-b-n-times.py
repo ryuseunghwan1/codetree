33e801fa017d464bf721c1cc16a837352ecbbfd1
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-21 00:14:28 +0900

    Title: ab곱 N번 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260421/ab곱 N번 구하기/a-multiple-b-n-times.py
@@ -0,0 +1,11 @@
+N = int(input())
+
+for _ in range(N):
+    sum_val = 1
+    arr = input().split()
+    a, b = int(arr[0]), int(arr[1])
+
+    for i in range(a, b + 1):
+        sum_val *= i
+    print(sum_val)
+
