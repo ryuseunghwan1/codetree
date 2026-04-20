c4b378245c374939e922cdf365cab96e8bde47a2
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-21 00:45:28 +0900

    Title: 약수가 세 개인 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260421/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 260421/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -2,7 +2,7 @@ start, end = map(int, input().split())
 
 cnt_1 = 0
 # Please write your code here.
-for i in range(start, end):
+for i in range(start, end+1):
     cnt = 0
     for j in range(1,i+1):
         if i % j == 0:
