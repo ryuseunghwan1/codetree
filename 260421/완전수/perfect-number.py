dc18fda9ccf38a9c983bfdb2f223f3c99a3da4c6
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-21 00:37:07 +0900

    Title: 완전수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260421/완전수/perfect-number.py
@@ -0,0 +1,16 @@
+start, end = map(int, input().split())
+
+cnt_1 = 0
+# Please write your code here.
+for i in range(start, end):
+    cnt = 0
+    for j in range(1,i):
+        if i % j == 0:
+            cnt += j
+    if cnt == i:
+        cnt_1 += 1
+        
+print(cnt_1)            
+        
+        
+    
