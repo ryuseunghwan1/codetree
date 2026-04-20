d6a407a8ce5b80450eab117a18323333acf3b8eb
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-21 00:44:50 +0900

    Title: 약수가 세 개인 수, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260421/약수가 세 개인 수/numbers-with-three-divisors.py
+++ 260421/약수가 세 개인 수/numbers-with-three-divisors.py
@@ -4,13 +4,17 @@ cnt_1 = 0
 # Please write your code here.
 for i in range(start, end):
     cnt = 0
-    for j in range(1,i):
+    for j in range(1,i+1):
         if i % j == 0:
-            cnt += j
+            cnt += 1
     if cnt == 3:
         cnt_1 += 1
 
 print(cnt_1)
+
+
         
+        
+    
         
     
