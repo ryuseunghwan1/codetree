ee7ecf49cd02642bbffbff7e158399c0abd61809
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-29 00:31:40 +0900

    Title: 배열 놀이, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260429/배열 놀이/play-with-array.py
@@ -0,0 +1,17 @@
+a, b = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for _ in range(b):
+    cnt = list(map(int, input().split()))
+
+    if cnt[0] == 1:
+        print(arr[cnt[1]-1])
+    elif cnt[0] == 3:
+        for i in range(cnt[1]-1, cnt[2]):
+            print(arr[i], end= ' ')
+    elif cnt[0] == 2:
+        print(arr.index(cnt[1])+1)
+    else:
+        print(0)
+        
+        
