11f863793778ef97e651e0bc997a2505f098202a
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-04-30 01:36:27 +0900

    Title: 배열의 평균, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260430/배열의 평균/ave-of-array.py
+++ 260430/배열의 평균/ave-of-array.py
@@ -4,12 +4,12 @@ for _ in range(2):
     cnt_arr = list(map(int, input().split()))
     arr.append(cnt_arr)
     
-print(sum(arr[0])/len(arr[0]), sum(arr[1])/len(arr[1]))
+print(round(sum(arr[0])/len(arr[0]),1), round(sum(arr[1])/len(arr[1]),1))
 
 for i in range(4):
-    print((arr[0][i] + arr[1][i])/ 2, end = ' ')
+    print(  round(  (arr[0][i] + arr[1][i])/ 2  ,1)   , end = ' ')
 print()
-print(sum(arr[0]+arr[1])/8)
+print(  round( sum(arr[0]+arr[1])/8,1)  )
 
 
 
