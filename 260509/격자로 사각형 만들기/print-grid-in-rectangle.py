a66b97d7b1f42902b4694d3c4449e93fd21637eb
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-09 02:20:17 +0900

    [Passed] 격자로 사각형 만들기 (47ms, 16MB)

--- 260509/격자로 사각형 만들기/print-grid-in-rectangle.py
+++ 260509/격자로 사각형 만들기/print-grid-in-rectangle.py
@@ -6,8 +6,8 @@ for i in range(n):
     arr_2d[0][i] = 1
     arr_2d[i][0] = 1
 
-for j in range(1, 5):
-    for m in range(1, 5):
+for j in range(1, n):
+    for m in range(1, n):
         arr_2d[j][m] = arr_2d[j-1][m] + arr_2d[j][m-1] + arr_2d[j-1][m-1]
 
 for arr in arr_2d:
