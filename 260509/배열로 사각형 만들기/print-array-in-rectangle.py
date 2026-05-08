465c4ce9e480b48273d13c0b9c78267f971d9687
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-09 02:03:13 +0900

    [Passed] 배열로 사각형 만들기 (45ms, 16MB)

---
+++ 260509/배열로 사각형 만들기/print-array-in-rectangle.py
@@ -0,0 +1,14 @@
+arr_2d = [[ 0 for _ in range(5) ] for _ in range(5)]
+
+for i in range(5):
+    arr_2d[0][i] = 1
+    arr_2d[i][0] = 1
+
+for j in range(1, 5):
+    for m in range(1, 5):
+        arr_2d[j][m] = arr_2d[j-1][m] + arr_2d[j][m-1]
+
+for arr in arr_2d:
+    for elem in arr:
+        print(elem, end=' ')
+    print()
