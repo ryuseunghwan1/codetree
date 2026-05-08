b4320eedc2b073f3850e5f06cee4a8c95680e0a5
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-09 02:34:24 +0900

    [Passed] 격자에 점 그리기 (47ms, 16MB)

---
+++ 260509/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,13 @@
+a, b = map(int, input().split())
+
+arr_2d = [[0 for _ in range(a)] for _ in range(a)]
+num = 1
+for _ in range(b):
+    r , c = tuple(map(int, input().split()))
+    arr_2d[r-1][c-1] = num
+    num += 1
+
+for i in arr_2d:
+    for elem in i:
+        print(elem, end=' ')
+    print()    
