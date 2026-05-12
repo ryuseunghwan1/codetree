a7fa74fd49c7153eee5edccf4ea0150f859581ac
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-13 02:32:39 +0900

    [Wrong Answer] 사는 지역 (45ms, 16MB)

---
+++ trail2/사는 지역/where-live.py
@@ -0,0 +1,12 @@
+n = int(input())
+name = []
+street_address = []
+region = []
+
+for _ in range(n):
+    n_i, s_i, r_i = input().split()
+    name.append(n_i)
+    street_address.append(s_i)
+    region.append(r_i)
+
+# Please write your code here.
