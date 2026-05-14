e92bf0cdbdc842085aa83a750a6b79beb5dc3fae
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-15 01:33:03 +0900

    [Wrong Answer] DateTime to DateTime (45ms, 16MB)

---
+++ trail2/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+d, e, f = 11*(24*60), 11*60, 11
+a, b, c = a*(24*60), (b*60), c
+
+total_1 = a + b + c
+total_2 = d + e + f
+
+if total_1 > total_2:
+    print(total_1 - total_2)
+else:
+    print(-1)
