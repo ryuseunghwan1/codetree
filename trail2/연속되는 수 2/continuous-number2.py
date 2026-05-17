e1db85f6b81592b6f18b7d8717dc412c4c2aa0c3
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-18 08:22:25 +0900

    [Wrong Answer] 연속되는 수 2 (62ms, 16MB)

--- trail2/연속되는 수 2/continuous-number2.py
+++ trail2/연속되는 수 2/continuous-number2.py
@@ -4,7 +4,7 @@ arr = [int(input()) for _ in range(n)]
 # Please write your code here.
 cnt = 0
 for i in range(n):
-    if i == 0 or arr[i-1] != arr[i]:
+    if i == 0 or arr[i-1] == arr[i]:
         cnt += 1
 
 print(cnt)
