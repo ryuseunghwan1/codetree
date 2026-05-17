5bc27f7f7983c66ffc6202d317d463be4bee5d57
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-17 13:03:58 +0900

    [Wrong Answer] 최대로 겹치는 구간 (45ms, 16MB)

--- trail2/최대로 겹치는 구간/maximum-overlapped-segments.py
+++ trail2/최대로 겹치는 구간/maximum-overlapped-segments.py
@@ -1,14 +1,12 @@
 n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-
-cnt = [0 for _ in range(101)]
+cnt = [0 for _ in range(201)]
 
 
 for a, b in segments:
     
-    for i in range(a, b + 1):
+    for i in range(100+a, 100+b+1):
         cnt[i] += 1
 
-print(max(cnt))
+max(cnt)
