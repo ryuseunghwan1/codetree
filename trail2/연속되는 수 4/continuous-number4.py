cd9a8a906dc839c0e99f236f20d17da3466c9556
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-18 08:35:11 +0900

    [Passed] 연속되는 수 4 (47ms, 16MB)

---
+++ trail2/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+
+ans = 0
+cnt = 0
+
+for i in range(n):
+    if i ==0 or arr[i] > arr[i-1]:
+        cnt += 1
+    else:
+        cnt = 1
+
+    ans = max(ans, cnt)
+
+print(ans)
