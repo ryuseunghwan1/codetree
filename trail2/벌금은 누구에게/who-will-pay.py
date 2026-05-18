06e8b339ea410c05a98383471a4d41e9136b0b71
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-18 09:00:03 +0900

    [Wrong Answer] 벌금은 누구에게 (83ms, 18MB)

---
+++ trail2/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,12 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Please write your code here.
+num = [0 for _ in range(N+1)]
+
+for i in student:
+    num[i] += 1
+
+for j in num:
+    if j >= K:
+        print(num.index(j))
