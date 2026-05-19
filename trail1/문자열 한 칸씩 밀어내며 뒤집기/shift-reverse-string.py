2270f67b60783eaba43e9961fe0082288cab7223
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-20 02:11:39 +0900

    [Wrong Answer] 문자열 한 칸씩 밀어내며 뒤집기 (47ms, 16MB)

---
+++ trail1/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.py
@@ -0,0 +1,15 @@
+input_str, q = input().split()
+q = int(q)
+queries = [int(input()) for _ in range(q)]
+
+# Please write your code here.
+for i in queries:
+    if i == 1:
+        input_str = input_str[1:] + input_str[0]
+        print(input_str)
+    elif i == 2:
+        input_str = input_str[-1] + input_str[:-1]
+        print(input_str)
+    else:
+        print(input_str[::-1])
+        
