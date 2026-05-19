c48c20503116118c12ea990e9e010e01988b6765
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-20 02:29:24 +0900

    [Passed] 특정 구간의 합 (55ms, 17MB)

--- trail2/특정 구간의 합/sum-of-specific-intervals.py
+++ trail2/특정 구간의 합/sum-of-specific-intervals.py
@@ -4,9 +4,11 @@ queries = [tuple(map(int, input().split())) for _ in range(m)]
 
 # Please write your code here.
 
+cnt = 0
+for i, j in queries:
+    for n in range(i, j+1):
+        cnt += arr[n-1]
+    print(cnt)
+    cnt = 0
 
-for a, b in queries:
-    total = 0
-    for j in range(a, b):
-        total += arr[j[j-1]
-    print(total)   
+
