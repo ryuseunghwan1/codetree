07a33f4f6a2196ab5781eb852e4615a0cd62bb0c
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-21 01:45:43 +0900

    [Wrong Answer] 원점으로부터의 거리 (44ms, 16MB)

--- trail2/원점으로부터의 거리/distance-from-origin.py
+++ trail2/원점으로부터의 거리/distance-from-origin.py
@@ -2,3 +2,11 @@ n = int(input())
 points = [(int(i), tuple(map(int, input().split()))) for i in range(n)]
 
 # Please write your code here.
+arr= []
+for i in range(n):
+    for j in range(2):
+        if points[i][1][j] < 0:
+            arr.append(-(points[i][1][j]))
+        else:
+            arr.append(points[i][1][j])
+
