1b72ca4fe46777a54583d210d6ed58d29ba7771c
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-25 14:12:58 +0900

    [Passed] 구역 청소 (51ms, 16MB)

--- trail2/구역 청소/district-cleaning.py
+++ trail2/구역 청소/district-cleaning.py
@@ -1,17 +1,20 @@
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-# Please write your code here.
-
 def clean(a, b, c, d):
     cnt_1 = b - a
     cnt_2 = d - c
-    cnt_3 = 0
     
-    if d > a:
-        cnt_3 = d - a
+    # 겹치는 구간의 시작점과 끝점을 구합니다.
+    overlap_start = max(a, c)
+    overlap_end = min(b, d)
+    
+    # 끝점이 시작점보다 크다면 실제로 겹치는 구역이 존재하는 것입니다.
+    if overlap_start < overlap_end:
+        cnt_3 = overlap_end - overlap_start
+    else:
+        cnt_3 = 0
     
     return cnt_1 + cnt_2 - cnt_3
 
 print(clean(a, b, c, d))
-
