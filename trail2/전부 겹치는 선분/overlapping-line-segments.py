10a3a79816545444e1fff012e39160e2e33cfb09
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-26 03:39:32 +0900

    [Passed] 전부 겹치는 선분 (56ms, 16MB)

--- trail2/전부 겹치는 선분/overlapping-line-segments.py
+++ trail2/전부 겹치는 선분/overlapping-line-segments.py
@@ -5,10 +5,13 @@ x1, x2 = list(x1), list(x2)
 
 # Please write your code here.
 
-def line(*args, *args):
-    if x2 in x1:
-        return 'Yes'
-    else:
-        return 'No'
+# 모든 선분의 시작점 중 가장 우측에 있는 점을 찾습니다.
+max_x1 = max(x1)
+# 모든 선분의 끝점 중 가장 좌측에 있는 점을 찾습니다.
+min_x2 = min(x2)
 
-print(line(x1 ,x2))
+# 가장 늦게 시작한 점이 가장 먼저 끝난 점보다 작거나 같다면 모든 선분이 겹치는 구간이 존재합니다.
+if max_x1 <= min_x2:
+    print("Yes")
+else:
+    print("No")
