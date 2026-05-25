ed4441633a5f3f1fc8233b000a953d08d784c5ee
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-26 04:00:23 +0900

    [Wrong Answer] 함수를 이용해 직사각형 만들기 (48ms, 16MB)

---
+++ trail2/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,9 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+
+def string():
+    for i in range(n):
+        for j in range(m):
+            print(j, end='')
+        print()
