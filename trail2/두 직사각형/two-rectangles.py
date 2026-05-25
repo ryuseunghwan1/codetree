006f6a5e95a9de55ba93a8ded27a3f7eb2fc2676
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-05-25 14:06:55 +0900

    [Passed] 두 직사각형 (51ms, 16MB)

--- trail2/두 직사각형/two-rectangles.py
+++ trail2/두 직사각형/two-rectangles.py
@@ -1,10 +1,9 @@
 x1, y1, x2, y2 = map(int, input().split())
 a1, b1, a2, b2 = map(int, input().split())
 
-# Please write your code here.
-
 def quar(x1, y1, x2, y2, a1, b1, a2, b2):
-    if x2 < a1 or a2 < x1:
+    # 좌, 우, 하, 상 4가지 방향 중 하나라도 완전히 떨어져 있다면 nonoverlapping
+    if x2 < a1 or a2 < x1 or y2 < b1 or b2 < y1:
         return 'nonoverlapping'
     else:
         return 'overlapping'
