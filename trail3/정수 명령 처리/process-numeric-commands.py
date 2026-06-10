98dcf55de194e7294e48d8a833bc725d62665925
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-06-11 01:20:32 +0900

    [Wrong Answer] 정수 명령 처리 (48ms, 16MB)

---
+++ trail3/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,13 @@
+N = int(input())
+command = []
+value = []
+
+for _ in range(N):
+    line = input().split()
+    command.append(line[0])
+    if line[0] == "push":
+        value.append(int(line[1]))
+    else:
+        value.append(0)
+
+# Please write your code here.
