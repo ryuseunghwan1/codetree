59634a64223b6f0d0fba8ef648679d855451d90c
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-06-17 00:27:35 +0900

    [Wrong Answer] 함수를 이용한 소수 판별 (50ms, 16MB)

--- trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ trail2/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,3 +1,20 @@
 a, b = map(int, input().split())
 
 # Please write your code here.
+
+def is_prime(n):
+    if n == 1:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+
+sum_val = 0
+for i in range(a, b+1):
+    if is_prime(i):
+        sum_val += i
+
