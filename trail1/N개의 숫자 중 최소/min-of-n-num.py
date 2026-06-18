2dd7d367359a0ddf95f50448487731a1e42b6768
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-06-19 01:50:15 +0900

    [Runtime Error] N개의 숫자 중 최소 (85ms, 18MB)

--- trail1/N개의 숫자 중 최소/min-of-n-num.py
+++ trail1/N개의 숫자 중 최소/min-of-n-num.py
@@ -1,13 +1,16 @@
-import sys
-
 n = int(input())
 a = list(map(int, input().split()))
 
 # Please write your code here.
+min_val = arr[0]
+cnt = 1
 
-count = sys.maxsize
-for i in a:
-    if i < count:
-        count = i
+for i in ragne(1, n):
+    if min_val > arr[i]:
+        min_val = arr[i]
+        cnt = 1
+    elif min_val == arr[i]:
+        cnt += 1
 
-print(count, a.count(count))
+
+print(min_val, cnt)
