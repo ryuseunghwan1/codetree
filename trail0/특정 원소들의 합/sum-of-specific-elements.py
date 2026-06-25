4ab41f11c1c494bd22cd34ac7147a5b3f71b5471
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-06-25 12:50:43 +0900

    [Passed] 특정 원소들의 합 (54ms, 16MB)

---
+++ trail0/특정 원소들의 합/sum-of-specific-elements.py
@@ -0,0 +1,9 @@
+arr = [list(map(int, input().split())) for _ in range(4)]
+total = 0
+
+for i in range(4):
+    for j in range(4):
+        if i >= j:
+            total += arr[i][j]
+        
+print(total)
