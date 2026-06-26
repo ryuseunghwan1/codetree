ab94f4f2a26e30fc4335877718087d154d5a0662
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-06-26 12:28:03 +0900

    [Passed] 연속되는 수 3 (71ms, 16MB)

--- trail2/연속되는 수 3/continuous-number3.py
+++ trail2/연속되는 수 3/continuous-number3.py
@@ -1,18 +1,21 @@
-N = int(input())
-arr = [int(input()) for _ in range(N)]
+# 변수 선언 및 입력
+n = int(input())
+arr = [
+    int(input())
+    for _ in range(n)
+]
 
-# Please write your code here.
-ans = 0
-cnt = 0
-
-for i in range(N):
-    if i == 0 or (arr[i] > 0 and arr[i-1] > 0):
+# 연속하여 동일한 숫자가 나오는 횟수를 구해보며,
+# 그 중 최댓값을 갱신합니다.
+ans, cnt = 0, 0
+for i in range(n):
+    # Case 1
+    if i >= 1 and arr[i] * arr[i - 1] > 0:
         cnt += 1
-    elif i == 0 or (arr[i] < 0 and arr[i-1] < 0):
-        cnt += 1
+    # Case 2
     else:
         cnt = 1
-
+    
     ans = max(ans, cnt)
 
 print(ans)
