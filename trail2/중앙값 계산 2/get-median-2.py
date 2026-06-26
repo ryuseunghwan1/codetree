85e8fc3aaf55410f43766c51dea2f887681e8697
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-06-27 05:24:46 +0900

    [Passed] 중앙값 계산 2 (48ms, 16MB)

---
+++ trail2/중앙값 계산 2/get-median-2.py
@@ -0,0 +1,17 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+cnt = 0
+cnt_1 = 0
+arr_1 = []
+
+for i in arr:
+    cnt += 1
+    arr_1.append(i)
+    if cnt % 2 != 0:
+        arr_1.sort()
+        print(arr_1[cnt_1], end=' ')
+        cnt_1 += 1
+    
+    
