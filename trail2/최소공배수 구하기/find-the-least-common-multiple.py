360725b38d8b05019da8cb0d71b14137f5896487
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-04 01:08:38 +0900

    [Passed] 최소공배수 구하기 (49ms, 16MB)

--- trail2/최소공배수 구하기/find-the-least-common-multiple.py
+++ trail2/최소공배수 구하기/find-the-least-common-multiple.py
@@ -1,15 +1,15 @@
-n, m = map(int, input().split())
-
-# Please write your code here.
-
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-def lcm(a, b):
-    return (a * b) // gcd(a, b)
+# 변수 선언 및 입력:
+n, m = tuple(map(int, input().split()))
 
 
-# 출력
-print(lcm(n, m))
+# n과 m의 최소공배수를 출력합니다.
+def find_lcm(n, m):
+    gcd = 0
+    for i in range(1, min(n, m) + 1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+
+    print(n * m // gcd)
+   
+
+find_lcm(n, m)
