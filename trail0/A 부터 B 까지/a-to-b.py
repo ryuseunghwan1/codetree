6e431495fc21e10fc528b8eaeb2121de1c5d35ef
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-16 02:02:15 +0900

    [Passed] A 부터 B 까지 (51ms, 16MB)

---
+++ trail0/A 부터 B 까지/a-to-b.py
@@ -0,0 +1,14 @@
+num1, num2 = map(int, input().split())
+
+while num1 >= 0:
+    
+    print(num1, end=" ")
+    
+    if num1 % 2 == 0: 
+        num1 += 3 
+    else:
+        num1 *= 2
+        
+    if num1 > num2:
+        break
+    
