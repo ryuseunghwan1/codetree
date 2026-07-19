f5fc0ef943131baa057d9a4b6b9b9999f43f3207
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-20 01:39:46 +0900

    [Passed] 배열의 합 (63ms, 16MB)

--- trail0/배열의 합/sum-of-array.py
+++ trail0/배열의 합/sum-of-array.py
@@ -1,22 +1,12 @@
-matrix = [list(map(int,input().split())) for _ in range(4)]
-
-a = 0
-b = 0
-c= 0
-d =0
-
+# 2차원 배열을 구현해 각 줄마다 정수를 입력받습니다.
+arr_2d = [
+    list(map(int, input().split()))
+    for _ in range(4)
+]
+    
+# 각 줄의 합을 출력합니다.
 for i in range(4):
+    ans = 0
     for j in range(4):
-        if i == 0:
-            a += matrix[i][j]
-        elif i == 1:
-            b += matrix[i][j]
-        elif i == 2:
-            c += matrix[i][j]
-        else:
-            d += matrix[i][j]
-
-print(a)
-print(b)
-print(c)
-print(a)
+        ans += arr_2d[i][j]
+    print(ans)
