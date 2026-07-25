145b7449cfcdc7c9e18b924ce6b6b1791cfa4ade
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-26 06:00:32 +0900

    [Passed] 한 가지로 열리는 자물쇠 (58ms, 17MB)

--- trail2/한 가지로 열리는 자물쇠/one-way-lock.py
+++ trail2/한 가지로 열리는 자물쇠/one-way-lock.py
@@ -1,11 +1,12 @@
 n = int(input())
 a, b, c = map(int, input().split())
 
-cnt = n ** 3
-for i in range(1, n+1):
-    for j in range(1, n+1):
-        for m in range(1, n+1):
-            if (i - a >= 3) and (j-b >= 3) and (m-c >=3):
-                cnt -= 1                
+cnt = 0
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        for m in range(1, n + 1):
+            # 세 자리 중 단 한 자리라도 거리가 2 이내인 경우
+            if abs(i - a) <= 2 or abs(j - b) <= 2 or abs(m - c) <= 2:
+                cnt += 1
 
 print(cnt)
