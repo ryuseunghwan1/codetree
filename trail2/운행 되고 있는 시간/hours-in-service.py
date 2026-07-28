0b767b4cdf08f07b18f6881ef494d3febd67690f
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-29 01:32:12 +0900

    [Passed] 운행 되고 있는 시간 (70ms, 17MB)

--- trail2/운행 되고 있는 시간/hours-in-service.py
+++ trail2/운행 되고 있는 시간/hours-in-service.py
@@ -1,22 +1,30 @@
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
 
-def calculate(a,b):
-    total = b - a
-    return total
+max_time = 0
 
-cnt = 0
+# i번 개발자를 해고한다고 가정
 for i in range(n):
+    # 각 시간대별로 일하는 사람이 있는지 체크하기 위한 배열 (시간은 최대 1000까지)
+    time_count = [0] * 1005
+    
+    # i번을 제외한 나머지 개발자들의 작업 시간을 체크
     for j in range(n):
         if i == j:
             continue
-
-        first = calculate(points[i][0], points[i][1])
-        second = calculate(points[j][0], points[j][1])
-        total = first + second
-
-        cnt = max(total, cnt)
-
-print(cnt-1)
-
         
+        start, end = points[j]
+        # [start, end) 구간에 속하는 시간 단위에 카운트 증가
+        for t in range(start, end):
+            time_count[t] += 1
+            
+    # 직원이 1명이라도 일하고 있는 시간(합집합)의 총합 계산
+    current_time = 0
+    for t in range(1, 1000):
+        if time_count[t] > 0:
+            current_time += 1
+            
+    # 최댓값 갱신
+    max_time = max(max_time, current_time)
+
+print(max_time)
