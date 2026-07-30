36af2dbf40c38951a0edbf2d85b6c4a167cd8b64
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-30 09:49:49 +0900

    [Runtime Error] 숫자들의 합 중 최대 (100ms, 19MB)

--- trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -7,3 +7,15 @@ for i in range(x, y+1):
     cnt = max(cnt, result)
 
 print(cnt)
+
+x, y = map(int, input().split())
+
+cnt = 0
+for i in range(x, y + 1):
+    s = str(i).zfill(2)  
+    a, b = int(s[0]), int(s[1])
+
+    result = a + b
+    cnt = max(cnt, result)
+
+print(cnt)
