99277e6653491127ab240ac427b7c0b8950dc766
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-07-30 09:50:06 +0900

    [Wrong Answer] 숫자들의 합 중 최대 (58ms, 17MB)

--- trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
+++ trail2/숫자들의 합 중 최대/maximum-of-sum-of-numbers.py
@@ -1,16 +1,6 @@
 x, y = map(int, input().split())
 
 cnt = 0
-for i in range(x, y+1):
-    a, b = tuple(map(int, list(str(i))))
-    result = a + b
-    cnt = max(cnt, result)
-
-print(cnt)
-
-x, y = map(int, input().split())
-
-cnt = 0
 for i in range(x, y + 1):
     s = str(i).zfill(2)  
     a, b = int(s[0]), int(s[1])
