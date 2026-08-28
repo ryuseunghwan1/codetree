b20c6debdd07d6a6f0dafb48fba74869ce3bbd93
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-08-28 16:36:55 +0900

    [Passed] 총점 비교 (54ms, 16MB)

--- trail2/총점 비교/compare-total-points.py
+++ trail2/총점 비교/compare-total-points.py
@@ -16,5 +16,5 @@ for _ in range(n):
 students.sort(key = lambda x : x.kor + x.eng + x.math)
 
 for student in students:
-    print(student.name, student.kor, student.eng, student.math
+    print(student.name, student.kor, student.eng, student.math)
     
