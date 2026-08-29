7e0c5513f97b3d1bb1145eb5c270d4e93db358b2
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-08-29 16:38:21 +0900

    [Passed] 줄 세우기 2 (107ms, 18MB)

--- trail2/줄 세우기 2/line-up-students-2.py
+++ trail2/줄 세우기 2/line-up-students-2.py
@@ -16,4 +16,4 @@ for i in range(1, n+1):
 students.sort(key= lambda x: (x[0].height, -x[0].weight))
 
 for j in students:
-    print(j[0].height, j[0].weight, j[1]
+    print(j[0].height, j[0].weight, j[1])
