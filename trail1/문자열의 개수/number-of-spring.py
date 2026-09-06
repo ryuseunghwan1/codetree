0b6c94ff0a9e9025fa9b1aec296eeb6c859e738c
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-06 10:51:38 +0900

    [Passed] 문자열의 개수 (48ms, 16MB)

--- trail1/문자열의 개수/number-of-spring.py
+++ trail1/문자열의 개수/number-of-spring.py
@@ -1,18 +1,18 @@
-
-total = []
 cnt = 0
+total = []
 
 while True:
     n = input()
 
-    if n = '0':
-        break
-    else:
-        total.append(n)
+    if n != '0':
         cnt += 1
-        
+        total.append(n)
+    else:
+        break
+
 print(cnt)
 
 for i in range(len(total)):
     if i % 2 == 0:
         print(total[i])
+
