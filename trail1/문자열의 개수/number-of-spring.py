ce1f52504c8d7421bf67d54a54397ad69c8de75f
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-06 10:50:19 +0900

    [Runtime Error] 문자열의 개수 (89ms, 19MB)

--- trail1/문자열의 개수/number-of-spring.py
+++ trail1/문자열의 개수/number-of-spring.py
@@ -1,15 +1,16 @@
-cnt = 0
+
 total = []
+cnt = 0
 
 while True:
     n = input()
 
-    if n != '0':
-        cnt += 1
-        total.append(n)
-    else:
+    if n = '0':
         break
-
+    else:
+        total.append(n)
+        cnt += 1
+        
 print(cnt)
 
 for i in range(len(total)):
