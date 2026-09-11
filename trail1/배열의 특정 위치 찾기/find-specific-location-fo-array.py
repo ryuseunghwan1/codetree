20e4859efb99442259151fe6a6851b1aa39d8e51
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-11 10:56:01 +0900

    [Passed] 배열의 특정 위치 찾기 (49ms, 16MB)

---
+++ trail1/배열의 특정 위치 찾기/find-specific-location-fo-array.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+even = 0
+arr_2 = []
+for i in arr[1::2]:
+    even += i 
+
+for j in arr[2::3]:
+    arr_2.append(j)
+
+print(even, round(sum(arr_2)/len(arr_2),1 ))
