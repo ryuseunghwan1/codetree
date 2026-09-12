7c7e875d9a7644a388455cc124cef7b7efae6967
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-13 01:38:41 +0900

    [Runtime Error] 10개 입력받기 (82ms, 18MB)

---
+++ trail1/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,21 @@
+arr = list(map(int, input().split()))
+arr_1 = []
+
+for i in arr:
+    if i != 0:
+        arr_1.append(i)      
+    else:
+        break
+
+print(sum(arr_1), round(sum(arr_1)/len(arr_1),1))
+
+arr = list(map(int, input().split()))
+arr_1 = []
+
+for i in arr:
+    if i != 0:
+        arr_1.append(i)
+    else:
+        break
+
+print(sum(arr_1), round(sum(arr_1)/len(arr_1),1))
