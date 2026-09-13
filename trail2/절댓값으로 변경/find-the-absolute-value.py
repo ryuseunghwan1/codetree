d0da64cbfbeefedc8573d61dbee4d736459f643e
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-14 06:29:37 +0900

    [Passed] 절댓값으로 변경 (52ms, 16MB)

--- trail2/절댓값으로 변경/find-the-absolute-value.py
+++ trail2/절댓값으로 변경/find-the-absolute-value.py
@@ -1,14 +1,16 @@
+
+
 n = int(input())
 arr = list(map(int, input().split()))
-
-# Please write your code here.
 
 def make_abs(arr):
     for i in range(n):
         arr[i] = abs(arr[i])
 
+    return
+
 make_abs(arr)
 
-for i in arr:
-    print(i, end=' ')
-
+for elem in arr:
+    print(elem, end= ' ')
+print()
