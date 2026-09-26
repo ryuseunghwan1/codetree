d6c03c447f8f2ef611ea19a9fce9247c4c19ca38
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-26 21:20:36 +0900

    [Wrong Answer] 체크판위에서 2 (46ms, 16MB)

--- trail2/체크판위에서 2/on-the-checkboard-2.py
+++ trail2/체크판위에서 2/on-the-checkboard-2.py
@@ -1,4 +1,22 @@
 R, C = map(int, input().split())
 grid = [list(input().split()) for _ in range(R)]
 
-# Please write your code here.
+ans = 0
+
+# 중간에 거쳐 갈 2개의 점(r1, c1)과 (r2, c2)를 완전탐색으로 찾습니다.
+for r1 in range(R):
+    for c1 in range(C):
+        for r2 in range(r1 + 1, R):
+            for c2 in range(c1 + 1, C):
+                # 1. 시작점(0,0) -> 첫 번째 점(r1,c1)
+                # 2. 첫 번째 점(r1,c1) -> 두 번째 점(r2,c2)
+                # 3. 두 번째 점(r2,c2) -> 도착점(R-1, C-1)
+                # 위 이동 과정에서 각각 칸의 색깔이 모두 달라야 합니다.
+                if (
+                    grid[0][0] != grid[r1][c1] and
+                    grid[r1][c1] != grid[r2][c2] and
+                    grid[r2][c2] != grid[R - 1][C - 1]
+                ):
+                    ans += 1
+
+print(ans)
