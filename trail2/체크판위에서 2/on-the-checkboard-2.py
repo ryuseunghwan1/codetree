7789cacc3e3416680d0c6b28bc4d5d1960aa9cc8
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-27 01:40:55 +0900

    [Passed] 체크판위에서 2 (50ms, 16MB)

--- trail2/체크판위에서 2/on-the-checkboard-2.py
+++ trail2/체크판위에서 2/on-the-checkboard-2.py
@@ -1,18 +1,32 @@
-R, C = map(int, input().split())
-grid = [list(input().split()) for _ in range(R)]
+import sys
 
-ans = 0
+def main():
+    data = sys.stdin.read().split()
+    R, C = int(data[0]), int(data[1])
+    # 칸이 공백으로 구분되든 붙어 있든 처리
+    s = "".join(data[2:])
+    grid = [s[i*C:(i+1)*C] for i in range(R)]
 
-for r1 in range(R):
-    for c1 in range(C):
-        for r2 in range(r1 + 1, R):
-            for c2 in range(c1 + 1, C):
-                # 이동할 때마다 색이 서로 달라야 함 (번갈아 가며 바뀌어야 함)
-                if (
-                    grid[0][0] != grid[r1][c1] and
-                    grid[r1][c1] != grid[r2][c2] and
-                    grid[r2][c2] != grid[R - 1][C - 1]
-                ):
-                    ans += 1
+    start, end = grid[0][0], grid[R-1][C-1]
+    if start == end:
+        print(0)
+        return
 
-print(ans)
+    # cnt[i][j] = (1..i-1, 1..j-1)이 아니라,
+    # 행 0..i-1, 열 0..j-1 영역에서 '중간1 후보'(시작과 다른 색, 첫 행/열 제외) 개수
+    cnt = [[0] * (C + 1) for _ in range(R + 1)]
+    for i in range(R):
+        for j in range(C):
+            is_p1 = 1 if (i >= 1 and j >= 1 and grid[i][j] != start) else 0
+            cnt[i+1][j+1] = cnt[i][j+1] + cnt[i+1][j] - cnt[i][j] + is_p1
+
+    ans = 0
+    # 중간2: 시작과 같은 색, 마지막 행/열 제외
+    for i in range(1, R - 1):
+        for j in range(1, C - 1):
+            if grid[i][j] == start:
+                # 행 0..i-1, 열 0..j-1 에 있는 중간1 후보 수
+                ans += cnt[i][j]
+    print(ans)
+
+main()
