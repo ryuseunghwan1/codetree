77319285ead1ec292180b44728f4d0991790f9b0
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-26 20:50:15 +0900

    [Passed] 일렬로 서있는 소 2 (55ms, 17MB)

--- trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
+++ trail2/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.py
@@ -3,10 +3,11 @@ A = list(map(int, input().split()))
 
 # Please write your code here.
 cnt = 0
+
 for i in range(n):
     for j in range(i+1, n):
         for m in range(j+1, n):
-            if A[i] <= A[j] <= A[m]:
+            if A[m] >= A[j] >= A[i]:
                 cnt += 1
 
 print(cnt)
