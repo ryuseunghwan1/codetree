a99f8ffae6155e97bdac657a4aafb8ee8e3e71f7
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-26 21:14:57 +0900

    [Passed] $1 \times 3$ 안의 최대 동전 수 (49ms, 16MB)

--- trail2/$1 \times 3$ 안의 최대 동전 수/best-place-of-13.py
+++ trail2/$1 \times 3$ 안의 최대 동전 수/best-place-of-13.py
@@ -1,15 +1,16 @@
 n = int(input())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-# Please write your code here.
-max_conins = 0
+max_coins = 0
 
+# 모든 행(i)과 3칸 이상 확보할 수 있는 열(j)을 순회합니다.
 for i in range(n):
-    for j in range(n-2):
+    for j in range(n - 2):
+        # 1 x 3 직사각형 범위 내의 동전 개수를 셉니다.
+        current_coins = grid[i][j] + grid[i][j + 1] + grid[i][j + 2]
         
-        current_coins = grid[i][j] + grid[i][j+1] + grid[i][j+2]
-
+        # 최대값 갱신
         if current_coins > max_coins:
             max_coins = current_coins
 
-print(current_coins)
+print(max_coins)
