4589d4a287391f2ce748ccc29bec25996fbba161
ryuseunghwan1 <78459269+ryuseunghwan1@users.noreply.github.com>
2026-09-26 20:29:36 +0900

    [Passed] 모이자 (56ms, 17MB)

--- trail2/모이자/gather.py
+++ trail2/모이자/gather.py
@@ -1,21 +1,16 @@
 import sys
 
-INT_MAX = sys.maxsize
+cnt = sys.maxsize
 
-# 변수를 선언하고 입력을 받습니다.
 n = int(input())
 arr = list(map(int, input().split()))
 
-min_dist = INT_MAX
-# 각 i번째 집으로 모였을 때의 합을 구해줍니다.
+
 for i in range(n):
-    sum_dist = 0
+    temp = 0
+
     for j in range(n):
-        sum_dist += abs(j - i) * arr[j]
+        temp += abs(j - i) * arr[j]
+    cnt = min(cnt, temp)
 
-    # 가능한 거리의 합 중 최솟값을 구해줍니다.
-    min_dist = min(min_dist, sum_dist)
-
-# 결과를 출력합니다.
-print(min_dist)
-
+print(cnt)
